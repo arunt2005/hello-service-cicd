b44443edeafc20a52b6e3e45cc0285f38ddd3dbe
arunt2005 <arunt2005@gmail.com>
2025-12-24 10:32:38 +0530

    sop added

--- src/main/java/com/apps/controller/HelloController.java
+++ src/main/java/com/apps/controller/HelloController.java
@@ -17,6 +17,7 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<String> greetMsg() {
+        System.out.println("hello service.");
         String msg = "hello service. welcome. - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy, hh:mm:ss:SSS a"));
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_PLAIN);
