b18c96a07050e6d86b4e86ab12ed2336c9f54165
arunt2005 <arunt2005@gmail.com>
2025-12-24 10:30:19 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/apps/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.apps.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> greetMsg() {
+        String msg = "hello service. welcome. - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy, hh:mm:ss:SSS a"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(msg, headers, HttpStatus.OK);
+    }
+
+}
